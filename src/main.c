5b68eae65ae56f74b806987b7b879ae636b8b6a3
wkalka <wkalka@agh.edu.pl>
2024-01-23 22:09:41 +0100

    [wip]

--- src/main.c
+++ src/main.c
@@ -14,7 +14,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define CPU_LOAD
+// #define CPU_LOAD
 // #define ADC_VBATT
 // #define ADXL345
 // #define DPS310
@@ -23,7 +23,7 @@
 #define SLEEP_MS	5000
 #endif
 #ifdef ADC_VBATT
-#define SLEEP_MS	5000
+#define SLEEP_MS	10000
 #endif
 #ifdef ADXL345
 #define SLEEP_MS	2000
@@ -31,6 +31,63 @@
 #ifdef DPS310
 #define SLEEP_MS	2000
 #endif
+
+
+K_THREAD_STACK_DEFINE(_threadStack, 1024);
+static struct k_thread _thread;
+static k_tid_t _threadID;
+
+static void _threadFunction(void* arg1, void* arg2, void* arg3)
+{
+    ARG_UNUSED(arg1);
+    ARG_UNUSED(arg2);
+    ARG_UNUSED(arg3);
+
+    uint32_t counter = 1;
+    // struct sensor_value sensorValue[3] = {0};
+    // double measurement[3] = {0};
+    struct sensor_value sensorValue = {0};
+    double measurement = 0;
+	// const struct device *const dev = DEVICE_DT_GET_ONE(adi_adxl345);
+    const struct device *const dev = DEVICE_DT_GET_ONE(infineon_dps310);
+    // if (!uChaosSensor_Create("adxl345", dev))
+    if (!uChaosSensor_Create("dps310", dev))
+    {
+        return;
+    }
+    int64_t lasTime = 0;
+    int64_t nowTime = 0;
+    uint64_t totalTime = 0;
+
+	while (1)
+    {
+        if (sensor_sample_fetch(dev) < 0)
+        {
+            printk("Sample fetch error!\r\n");
+        }
+        // if (sensor_channel_get(dev, SENSOR_CHAN_ACCEL_XYZ, &sensorValue[0]) < 0)
+        if (sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &sensorValue) < 0)
+        {
+            printk("Channel get error!\r\n");
+        }
+        // measurement[0] = sensor_value_to_double(&sensorValue[0]);
+        // measurement[1] = sensor_value_to_double(&sensorValue[1]);
+        // measurement[2] = sensor_value_to_double(&sensorValue[2]);
+        measurement = sensor_value_to_double(&sensorValue);
+        nowTime = k_uptime_get();
+        totalTime += (nowTime-lasTime);
+        // printk("%d;%f;%f;%f;%lld;%lld\r\n", counter, measurement[0], measurement[1], measurement[2], (nowTime-lasTime), totalTime);
+        printk("%d;%f;%lld;%lld\r\n", counter, measurement, (nowTime-lasTime), totalTime);
+        lasTime = nowTime;
+        counter++;
+        k_sleep(K_MSEC(2000));
+	}
+}
+
+static void thread(void* arg1, void* arg2, void* arg3)
+{
+    _threadFunction(arg1, arg2, arg3);
+}
 
 
 int main(void)
@@ -41,12 +98,18 @@ int main(void)
 
 #ifdef CPU_LOAD   
     uChaosCPU_Init();
+    _threadID = k_thread_create(&_thread, _threadStack,
+                                K_THREAD_STACK_SIZEOF(_threadStack),
+                                thread,
+                                NULL, NULL, NULL,
+                                12, 0, K_NO_WAIT);
 #endif
 #ifdef ADC_VBATT
     uChaosBattery_Init();
 #endif
 
 #ifdef ADC_VBATT
+    uint32_t counter = 1;
     int32_t val_mv;
     int error;
 	uint16_t buf;
@@ -87,7 +150,6 @@ int main(void)
 	const struct device *const dps310 = DEVICE_DT_GET_ONE(infineon_dps310);
 #endif
 
-	
 	while (1)
     {
 #ifdef ADC_VBATT
@@ -99,7 +161,8 @@ int main(void)
         }
         val_mv = (int32_t)buf;
         error = adc_raw_to_millivolts_dt(&adc_channel, &val_mv);
-        printk("VBATT = %"PRId32" mV\r\n", val_mv);
+        printk("%d;%"PRId32"\r\n", counter, val_mv);
+        counter++;
         k_sleep(K_MSEC(SLEEP_MS));
 #endif
 #ifdef ADXL345
@@ -114,7 +177,8 @@ int main(void)
         measurement[0] = sensor_value_to_double(&sensorValue[0]);
         measurement[1] = sensor_value_to_double(&sensorValue[1]);
         measurement[2] = sensor_value_to_double(&sensorValue[2]);
-        printk("%d X = %f;Y = %f;Z = %f\r\n", counter++, measurement[0], measurement[1], measurement[2]);
+        printk("%d;%f;%f;%f\r\n", counter, measurement[0], measurement[1], measurement[2]);
+        counter++;
         k_sleep(K_MSEC(SLEEP_MS));
 #endif
 #ifdef DPS310
@@ -127,7 +191,8 @@ int main(void)
             printk("Channel get error!\r\n");
         }
         measurement = sensor_value_to_double(&sensorValue);
-        printk("%d Temp = %f\r\n", counter++, measurement);
+        printk("%d;%f\r\n", counter, measurement);
+        counter++;
         k_sleep(K_MSEC(SLEEP_MS));
 #endif
 #ifdef CPU_LOAD
--- src/uchaos/uchaos_cpu/uchaos_cpu.c
+++ src/uchaos/uchaos_cpu/uchaos_cpu.c
@@ -1,43 +1,79 @@
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/sys/time_units.h>
+
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+
 #include "uchaos_cpu.h"
 
 
 K_THREAD_STACK_DEFINE(uChaosCPU_ThreadStack1, 512);
 K_THREAD_STACK_DEFINE(uChaosCPU_ThreadStack2, 512);
 K_THREAD_STACK_DEFINE(uChaosCPU_ThreadStack3, 512);
+K_THREAD_STACK_DEFINE(uChaosCPU_ThreadStack4, 512);
+K_THREAD_STACK_DEFINE(uChaosCPU_ThreadStack5, 512);
+K_THREAD_STACK_DEFINE(uChaosCPU_ThreadStack6, 512);
+K_THREAD_STACK_DEFINE(uChaosCPU_ThreadStack7, 512);
+K_THREAD_STACK_DEFINE(uChaosCPU_ThreadStack8, 512);
 
-// k_thread_stack_t * stacks [THREADS_NUMBER];
 static struct k_thread _uChaosCPU_Thread[THREADS_NUMBER];
 static k_tid_t _uChaosCPU_ThreadID[THREADS_NUMBER];
 static uint8_t _uChaosCPU_ThreadPriority[THREADS_NUMBER] = 
 {
-    10,
     12,
-    14
+    12,
+    12,
+    12,
+    12,
+    12,
+    12,
+    12
 };
 char* _uChaosThread_Names[] = 
 {
     "Thread1",
     "Thread2",
-    "Thread3"
+    "Thread3",
+    "Thread4",
+    "Thread5",
+    "Thread6",
+    "Thread7",
+    "Thread8"
 };
 static const char* _currentThreadName = NULL;
+
+static __thread uint32_t loopCounter;
+static __thread int64_t upTime;
+static __thread int64_t nowTime;
 
 
 static void uChaosCPU_ThreadFunction(const char* threadName, void* arg2, void* arg3)
 {
     ARG_UNUSED(arg2);
     ARG_UNUSED(arg3);
-	static uint32_t x = 0;
 
+    uint32_t loopDelayTimeMs = 300;
+    uint32_t threadSleepTimeMs = 1000;
+
+    upTime = k_uptime_get();
+    nowTime = k_uptime_get();
 	while (1)
     {
-        if ( x == UINT32_MAX)
+        while((nowTime - upTime) < loopDelayTimeMs)
         {
-            x = 0;
+            loopCounter++;
+            if ( loopCounter == UINT32_MAX)
+            {
+                loopCounter = 0;
+            }
+            nowTime = k_uptime_get();
         }
-        x++;
+        k_sleep(K_MSEC(threadSleepTimeMs));
+        upTime = k_uptime_get();
         printk("Thread uChaosCPU: %s\r\n", threadName);
-        k_sleep(K_MSEC(1000));
 	}
 }
 
@@ -50,13 +86,43 @@ static void uChaosCPU_Thread1(void* arg1, void* arg2, void* arg3)
 
 static void uChaosCPU_Thread2(void* arg1, void* arg2, void* arg3)
 {
-    uChaosCPU_ThreadFunction(_uChaosThread_Names[0], arg2, arg3);
+    uChaosCPU_ThreadFunction(_uChaosThread_Names[1], arg2, arg3);
 }
 
 
 static void uChaosCPU_Thread3(void* arg1, void* arg2, void* arg3)
 {
-    uChaosCPU_ThreadFunction(_uChaosThread_Names[0], arg2, arg3);
+    uChaosCPU_ThreadFunction(_uChaosThread_Names[2], arg2, arg3);
+}
+
+
+static void uChaosCPU_Thread4(void* arg1, void* arg2, void* arg3)
+{
+    uChaosCPU_ThreadFunction(_uChaosThread_Names[3], arg2, arg3);
+}
+
+
+static void uChaosCPU_Thread5(void* arg1, void* arg2, void* arg3)
+{
+    uChaosCPU_ThreadFunction(_uChaosThread_Names[4], arg2, arg3);
+}
+
+
+static void uChaosCPU_Thread6(void* arg1, void* arg2, void* arg3)
+{
+    uChaosCPU_ThreadFunction(_uChaosThread_Names[5], arg2, arg3);
+}
+
+
+static void uChaosCPU_Thread7(void* arg1, void* arg2, void* arg3)
+{
+    uChaosCPU_ThreadFunction(_uChaosThread_Names[6], arg2, arg3);
+}
+
+
+static void uChaosCPU_Thread8(void* arg1, void* arg2, void* arg3)
+{
+    uChaosCPU_ThreadFunction(_uChaosThread_Names[7], arg2, arg3);
 }
 
 
@@ -110,14 +176,49 @@ void uChaosCPU_Init(void)
                                             K_THREAD_STACK_SIZEOF(uChaosCPU_ThreadStack2),
                                             uChaosCPU_Thread2,
                                             NULL, NULL, NULL,
-                                            _uChaosCPU_ThreadPriority[i], 0, K_NO_WAIT);
+                                            _uChaosCPU_ThreadPriority[i], 0, K_FOREVER);
     k_thread_name_set(_uChaosCPU_ThreadID[i], _uChaosThread_Names[i]);
     i++;
     _uChaosCPU_ThreadID[i] = k_thread_create(&_uChaosCPU_Thread[i], uChaosCPU_ThreadStack3,
                                             K_THREAD_STACK_SIZEOF(uChaosCPU_ThreadStack3),
                                             uChaosCPU_Thread3,
                                             NULL, NULL, NULL,
-                                            _uChaosCPU_ThreadPriority[i], 0, K_NO_WAIT);
+                                            _uChaosCPU_ThreadPriority[i], 0, K_FOREVER);
+    k_thread_name_set(_uChaosCPU_ThreadID[i], _uChaosThread_Names[i]);
+    i++;
+    _uChaosCPU_ThreadID[i] = k_thread_create(&_uChaosCPU_Thread[i], uChaosCPU_ThreadStack4,
+                                            K_THREAD_STACK_SIZEOF(uChaosCPU_ThreadStack4),
+                                            uChaosCPU_Thread4,
+                                            NULL, NULL, NULL,
+                                            _uChaosCPU_ThreadPriority[i], 0, K_FOREVER);
+    k_thread_name_set(_uChaosCPU_ThreadID[i], _uChaosThread_Names[i]);
+    i++;
+    _uChaosCPU_ThreadID[i] = k_thread_create(&_uChaosCPU_Thread[i], uChaosCPU_ThreadStack5,
+                                            K_THREAD_STACK_SIZEOF(uChaosCPU_ThreadStack5),
+                                            uChaosCPU_Thread5,
+                                            NULL, NULL, NULL,
+                                            _uChaosCPU_ThreadPriority[i], 0, K_FOREVER);
+    k_thread_name_set(_uChaosCPU_ThreadID[i], _uChaosThread_Names[i]);
+    i++;
+    _uChaosCPU_ThreadID[i] = k_thread_create(&_uChaosCPU_Thread[i], uChaosCPU_ThreadStack6,
+                                            K_THREAD_STACK_SIZEOF(uChaosCPU_ThreadStack6),
+                                            uChaosCPU_Thread6,
+                                            NULL, NULL, NULL,
+                                            _uChaosCPU_ThreadPriority[i], 0, K_FOREVER);
+    k_thread_name_set(_uChaosCPU_ThreadID[i], _uChaosThread_Names[i]);
+    i++;
+    _uChaosCPU_ThreadID[i] = k_thread_create(&_uChaosCPU_Thread[i], uChaosCPU_ThreadStack7,
+                                            K_THREAD_STACK_SIZEOF(uChaosCPU_ThreadStack7),
+                                            uChaosCPU_Thread7,
+                                            NULL, NULL, NULL,
+                                            _uChaosCPU_ThreadPriority[i], 0, K_FOREVER);
+    k_thread_name_set(_uChaosCPU_ThreadID[i], _uChaosThread_Names[i]);
+    i++;
+    _uChaosCPU_ThreadID[i] = k_thread_create(&_uChaosCPU_Thread[i], uChaosCPU_ThreadStack8,
+                                            K_THREAD_STACK_SIZEOF(uChaosCPU_ThreadStack8),
+                                            uChaosCPU_Thread8,
+                                            NULL, NULL, NULL,
+                                            _uChaosCPU_ThreadPriority[i], 0, K_FOREVER);
     k_thread_name_set(_uChaosCPU_ThreadID[i], _uChaosThread_Names[i]);
 }
 
